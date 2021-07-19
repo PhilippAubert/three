1decb49695bbb89d704985ac4430eda5b9c6c3bf
philipp aubert <p.b.m.aubert@gmail.com>
2021-07-19 15:36:03 +0200

    fix animation

--- scene.js
+++ scene.js
@@ -13,14 +13,11 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-
-const animate = function () {
+function animate() {
 	requestAnimationFrame( animate );
-
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
 	renderer.render( scene, camera );
-};
-
+}
 animate();
+
