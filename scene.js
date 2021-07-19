fee218353b3264ea0dea738366b4bb9bb84d2715
philipp aubert <p.b.m.aubert@gmail.com>
2021-07-19 16:02:27 +0200

    Add animation to lines

--- scene.js
+++ scene.js
@@ -1,21 +1,30 @@
 import * as THREE from '/build/three.module.js'; 
 
+// BASIC RENDER SETUP
+
+const scene = new THREE.Scene();
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
+// CAMERA 
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 150 );
+camera.position.set( 0, 0, 20 );
 camera.lookAt( 0, 0, 0 );
 
-const scene = new THREE.Scene();
 
 const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 const points = [];
 points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 0, 20, 0 ) );
 points.push( new THREE.Vector3( 10, 0, 0 ) );
+points.push( new THREE.Vector3( 10, 10, 10 ) );
+points.push( new THREE.Vector3( 20, 10, 10 ) );
+points.push( new THREE.Vector3( -10, 0, 0 ) );
+
 
 const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
@@ -23,3 +32,12 @@ const line = new THREE.Line( geometry, material );
 
 scene.add( line );
 renderer.render( scene, camera );
+
+function animate(){
+	requestAnimationFrame(animate); 
+	line.rotation.x += 0.05; 
+	line.rotation.y += 0.01; 
+	renderer.render(scene, camera); 
+}
+
+animate()
